e45ba876ff7a5b7c78c268f65fa601899179e5cb
Wendy Wilgus <96785281+wendywilgus@users.noreply.github.com>
2023-04-24 17:53:00 -0400

    Added random card draw function

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-let age = 20
+// let age = 20
 
-if (age < 21) {
-    console.log("Sorry, you are too young to play!")
-} else {
-    console.log("Welcome to the Blackjack Game!")
-} 
+// if (age < 21) {
+//     console.log("Sorry, you are too young to play!")
+// } else {
+//     console.log("Welcome to the Blackjack Game!")
+// } 
 
 
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
-let gameOver = true
+let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let totalEl = document.getElementById("total-el");
 let cardsEl = document.getElementById("cards-el");
 
+//logic to get a random card back
+function getRandomCard() {
+    let randomCard = Math.floor( Math.random() * 13 ) + 1
+    return randomCard
+}
 
-function start() {
-    cardsEl.textContent = "Cards: 10 4 ";
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards:  " 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     totalEl.textContent = "Total: " + sum;
     if (sum < 21) {
         message = "Do you want to draw a new card?"
@@ -28,10 +41,17 @@ function start() {
         hasBlackJack = true
     } else if (sum > 21) {
         message = "You lose!"
-        gameOver = false
+        isAlive = false
     }
     messageEl.textContent = message
 }
 
-
+function newCard() {
+    messageEl.textContent = "Drawing a new card from the deck!"
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    console.log(cards)
+    renderGame();
+}
 
